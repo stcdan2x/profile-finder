8b5466f14ecba13dd97170012cb5814eba0278ce
Steven Dandan <stcdan2x@gmail.com>
2021-11-29 22:43:12 +0800

    added functionality for the search component to filter users according to typed criteria

--- src/App.js
+++ src/App.js
@@ -11,20 +11,37 @@ class App extends Component {
 		loading: false
 	};
 
-	async componentDidMount() {
+	/* async componentDidMount() {
 		this.setState({ loading: true });
 
 		const res = await axios.get("https://api.github.com/users");
 
 		this.setState({ users: res.data, loading: false });
-	}
+	} */
+
+	findUser = async (textVal) => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(`https://api.github.com/search/users?q=${textVal}`);
+		console.log(res.data.items);
+
+		this.setState({ users: res.data.items, loading: false });
+	};
+
+	clearSearchResults = () => this.setState({ users: [], loading: false });
 
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
 				<div className="container">
-					<Search />
+					<Search
+						users={this.state.users}
+						findUser={this.findUser}
+						clearSearchResults={this.clearSearchResults}
+						loading={this.state.loading}
+						showClearBtn={this.state.users.length > 0 ? true : false}
+					/>
 					<User users={this.state.users} loading={this.state.loading} />
 				</div>
 			</div>
--- src/components/elements/Search.js
+++ src/components/elements/Search.js
@@ -1,27 +1,81 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
 	state = {
-		text: ""
+		text: "",
+		message: "",
+		searchEmpty: ""
 	};
 
-	onChangeHandler = (e) => {
-		this.setState({ text: e.target.value });
+	static propTypes = {
+		findUser: PropTypes.func.isRequired,
+		clearSearchResults: PropTypes.func.isRequired
+	};
+
+	componentDidMount() {
+		this.setState({ searchEmpty: "" });
+	}
+
+	changeHandler = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	/* showMsg = async () => {
+		if (this.props.loading === false) {
+			if (this.props.users) {
+				await this.setState({ searchEmpty: "going" });
+
+				console.log(this.props.users);
+				console.log("yes");
+			} else {
+				await this.setState({ searchEmpty: "No matches found!" });
+
+				console.log(this.props.users);
+				console.log("no");
+			}
+		}
+	}; */
+
+	submitHandler = (e) => {
+		e.preventDefault();
+
+		if (!this.state.text) {
+			this.setState({ message: "Please type-in your search criteria!" });
+			return;
+		} else {
+			this.props.findUser(this.state.text);
+			this.setState({ text: "", message: "" });
+			if (this.props.loading === false) {
+				if (this.props.users > 0) {
+					this.setState({ searchEmpty: "going" });
+				} else if (!this.props.users) {
+					this.setState({ searchEmpty: "No matches found!" });
+				}
+			}
+		}
 	};
 
 	render() {
 		return (
 			<div>
-				<form className="form">
+				{this.state.message && <div className="notifier warning">{this.state.message}</div>}
+				{this.state.searchEmpty && (
+					<div className="notifier warning">{this.state.searchEmpty}</div>
+				)}
+				<form className="form" onSubmit={this.submitHandler}>
 					<input
 						type="text"
 						placeholder="Search User"
 						name="text"
 						value={this.state.text}
-						onChange={this.onChangeHandler}
+						onChange={this.changeHandler}
 					/>
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
+				<button className="btn btn-light btn-block" onClick={this.props.clearSearchResults}>
+					Clear
+				</button>
 			</div>
 		);
 	}
